1dc4718d61ee95372cfa4d9640b36587d9d7c540
Sagar Shrivastava <sagar.shrivastava6413@gmail.com>
2022-01-13 12:23:51 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 import { useEffect } from "react";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
@@ -35,6 +36,10 @@ function App() {
         <Switch>
           <Route path="/login">
             <Login />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Payment />
           </Route>
           <Route path="/checkout">
             <Header />
